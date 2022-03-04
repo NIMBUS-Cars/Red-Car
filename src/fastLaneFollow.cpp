8321f70a9ea87c52633ff428cb57b7b244a7b4b8
rbrown78137 <45879932+rbrown78137@users.noreply.github.com>
2022-03-04 16:22:08 -0600

    Update fastLaneFollow.cpp

--- src/fastLaneFollow.cpp
+++ src/fastLaneFollow.cpp
@@ -1,221 +1,216 @@
 #include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/cudafeatures2d.hpp>
+#include <sensor_msgs/Image.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
 #include <ackermann_msgs/AckermannDrive.h>
-#include <sensor_msgs/LaserScan.h>
-#include <string>
-#include <cmath>
+#include "torch/torch.h"
+#include "torch/script.h"
+#include "default_car/CarObject.h"
+#include "default_car/CarObjects.h"
 
-class ReactiveMethod {
-private:
-    // A ROS node
+using namespace std;
+using namespace cv;
+torch::Device device(torch::kCUDA);
+class LaneFollower{
+    private:
     ros::NodeHandle n;
-
-    // car parameters
-    double max_speed, max_steering_angle, car_width, car_length, max_acceleration,car_radius;
-
-    // Publish drive data
+    image_transport::ImageTransport image_transport;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
     ros::Publisher drive_pub;
-
-    //Listens for Laser Scan
-    ros::Subscriber scan_sub;
-
-    //float DISCREPANCY_DISTANCE = .4; 
-    float SCAN_WITHIN_ANGLE = ( 90 / 180.0) *0.95 * M_PI; //ADDED .95 for when it now checks for farthest angle based on how far it can go
-    double currentSpeed = 0;
-
-public:
-    ReactiveMethod() {
+    torch::jit::script::Module semanticSegmentationModule;
+    torch::jit::script::Module laneFollowModule;
+    torch::jit::script::Module speedControllerModule;
+    ros::Publisher object_detection_pub;
+    int camera_height,camera_width;
+    int desiredX = 256,desiredY=256;
+    int outputX = 1280, outputY = 720;
+    bool fastMode;
+    public:
+    LaneFollower():image_transport(n) {
         n = ros::NodeHandle("~");
-        std::string drive_topic, scan_topic;
-        n.getParam("/nav_drive_topic", drive_topic);
-        n.getParam("/scan_topic", scan_topic);
-        n.getParam("/max_speed", max_speed);
-        n.getParam("/max_steering_angle", max_steering_angle);
-        n.getParam("/width", car_width);
-        n.getParam("/wheelbase", car_length);
-        n.getParam("/max_decel",max_acceleration);
-        car_radius =sqrt(pow(car_width,2) +pow(car_length,2))/2;
+         n.getParam("/camera_height",camera_height);
+         n.getParam("/camera_width",camera_width);
+         std::string drive_topic, camera_topic, semanticSegmentationPath, laneFollowPath, imageOutput, object_detection_topic,speedControllerPath;
+         n.getParam("/in_fast_mode",fastMode);
+         n.getParam("/camera_topic",camera_topic);
+        image_sub_ = image_transport.subscribe(camera_topic, 1, &LaneFollower::image_callback, this);
+         n.getParam("/nav_drive_topic", drive_topic);
         drive_pub = n.advertise<ackermann_msgs::AckermannDriveStamped>(drive_topic, 10);
-        scan_sub = n.subscribe(scan_topic, 1, &ReactiveMethod::scan_callback, this);
+        n.getParam("/semantic_segmentation_path",semanticSegmentationPath);
+        n.getParam("/lane_follow_path",laneFollowPath);
+        n.getParam("/speed_controller_path",speedControllerPath);
+        n.getParam("/image_output",imageOutput);
+        image_pub_ = image_transport.advertise(imageOutput, 1);
+        n.getParam("/object_detection_topic",object_detection_topic);
+        object_detection_pub = n.advertise<default_car::CarObjects>(object_detection_topic,10);
+        // Load pytorch model
+        try {
+          semanticSegmentationModule = torch::jit::load(semanticSegmentationPath);
+          semanticSegmentationModule.to(device);
+          semanticSegmentationModule.eval();
+          laneFollowModule = torch::jit::load(laneFollowPath);
+          laneFollowModule.to(device);
+          laneFollowModule.eval();
+          speedControllerModule = torch::jit::load(speedControllerPath);
+          speedControllerModule.to(device);
+          speedControllerModule.eval();
+          }
+          catch (const c10::Error& e) {
+            ROS_ERROR("Error Loading Model: %s", e.what());
+          }
+
     }
-    void scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
-        //things to add
-        // speed that corresponds to how much you can travel with the aeb
-        std::vector<float> safeScanDistances(static_cast<int>(msg->ranges.size()));
-        for(int i = 0; i< static_cast<int>(msg->ranges.size());i++){
-            float test = msg->ranges.at(i);
-            safeScanDistances.at(i) = msg-> ranges.at(i);
-        }
-        for(int i = 0; i< static_cast<int>(msg->ranges.size());i++){
+    ~LaneFollower()
+  {
+  }
+    
+  void image_callback(const sensor_msgs::ImageConstPtr& msg){
+      cv_bridge::CvImagePtr cv_ptr;
+      Mat outputMat;
+      double steeringAngle = 0;
+      double carSpeed = 0;//1,75
+      default_car::CarObjects objectsDetected;
+      bool foundCar = false;
+      long minX= 256;
+      long maxX=0;
+      long minY = 256;
+      long maxY = 0;
+    try
+    {
+      float time1 = std::clock();
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGRA8);
+      Mat imageInRGB;
+      cvtColor(cv_ptr->image,imageInRGB,COLOR_RGBA2RGB);
 
-                double radiusToReduceValues = angleToAvoid(msg->ranges.at(i));
-                int indiciesToConsider = static_cast<int>(radiusToReduceValues / msg->angle_increment);
-                for(int j = 0; j< indiciesToConsider;j++){
-                    if(i-j>=0 && safeScanDistances.at(i-j)>msg->ranges.at(i)){
-                        safeScanDistances.at(i-j) = msg->ranges.at(i);
-                         float test = msg->ranges.at(i);
-                    }
-                    if(i+j<static_cast<int>(msg->ranges.size()) && safeScanDistances.at(i+j)>msg->ranges.at(i)){
-                        safeScanDistances.at(i+j)= msg->ranges.at(i);
-                         float test = msg->ranges.at(i);
-                    }
-                }
-        }
-        int indexOfFarthestValue = static_cast<int>(abs(msg->angle_min/msg->angle_increment));
-        float farthestDistance = safeScanDistances.at(indexOfFarthestValue);
-        for(int i = 0; i< static_cast<int>(msg->ranges.size());i++){
-            if(abs(msg->angle_increment * i + msg->angle_min) < SCAN_WITHIN_ANGLE){
-                if(safeScanDistances.at(i) > farthestDistance){
-                    // indexOfFarthestValue = i;
-                    // farthestDistance = safeScanDistances.at(i);
-                    //Checks to see if angle is ok for pure pursuit so it can choose other angles if the best angle isnt great
-                    
-                    double currentAngle = msg->angle_increment * i + msg->angle_min;
-                    double radiusOfCurvature = abs(pow(safeScanDistances.at(i),2) / (2 * safeScanDistances.at(i) * sin(currentAngle)));
-                    double steeringAngleForCurrentAngle = getSteeringAngleFromRadius(radiusOfCurvature,currentAngle);
-                    if(abs(steeringAngleForCurrentAngle) <max_steering_angle){
-                        indexOfFarthestValue = i;
-                        farthestDistance = safeScanDistances.at(i);
-                    }
-
-                }
-            }
-        }
-        double angleOfFarthestPoint = msg->angle_increment * indexOfFarthestValue + msg->angle_min;
-        // Pure Pursuit
-        double radiusOfCurvature;
-         int indexOfZero = static_cast<int>(abs(msg->angle_min/msg->angle_increment));
-         //replacing this for the length of the longest angle helps us avoid walls we might face that are between the 0 and the angle to the distance we are looking for that may make our car crash
-         //adjustments were made to base angle to max it the min of the distance in front of it so car can curve properly in long passage ways
-         double minimumAngleToConsider = -abs(atan(car_width/car_length));
-        if(minimumAngleToConsider<-M_PI){
-            minimumAngleToConsider = -M_PI;
-        }
-        double maximumAngleToConsider = abs(atan(car_width/car_length));
-        if(maximumAngleToConsider>M_PI){
-            maximumAngleToConsider = M_PI;
-        }
-        int indexOfMin = (minimumAngleToConsider /msg->angle_increment)+indexOfZero;
-        int indexOfMax = (maximumAngleToConsider /msg->angle_increment)+indexOfZero;
-        
-        double shortestDistanceInCurve = std::numeric_limits<double>::infinity();
-            if(angleOfFarthestPoint >0){
-                // was i = indexOfZero
-                for(int i = indexOfMin;i<indexOfFarthestValue;i++){
-                    if(shortestDistanceInCurve>msg->ranges.at(i)){
-                        shortestDistanceInCurve = msg->ranges.at(i);
-                    }
-                }
-            }else{
-                // was i< indexOfZero
-                for(int i = indexOfFarthestValue;i<indexOfMax;i++){
-                    if(shortestDistanceInCurve>msg->ranges.at(i)){
-                        shortestDistanceInCurve = msg->ranges.at(i);
-                    }
-                }
-            }
-        if(abs(angleOfFarthestPoint) < .95 * M_PI){
-        radiusOfCurvature = abs(pow(shortestDistanceInCurve,2) / (2 * shortestDistanceInCurve * sin(angleOfFarthestPoint)));
-        }else{
-            radiusOfCurvature = shortestDistanceInCurve -car_radius * 1.5;
-        }
-        double steeringAngle = getSteeringAngleFromRadius(radiusOfCurvature,angleOfFarthestPoint);
-        
-        // CALCULATE SPEED BASED ON TIME TO COLLISION DIRECTLY IN FRONT OF IT
-        double smallestTTC = std::numeric_limits<double>::infinity();
-        for(int i = indexOfMin;i<indexOfMax;i++){
-            if(i>=0 &&i<static_cast<int>(msg->ranges.size())){
-           if(abs(i*msg->angle_increment + msg->angle_min)<.1){
-              float distance = msg->ranges.at(i) - car_radius; // may need to remove car radius added as test
-              float velocity = currentSpeed; // may need to change back to currentSpeed 
-              float TTC = distance / velocity;
-              if(TTC<smallestTTC){
-                 smallestTTC=TTC;
+      //PREPROCESSING STEP
+      Mat resolutionReduced;
+      cv::resize(imageInRGB,resolutionReduced,Size(desiredX,desiredY));
+      //DATA FORMAT FOR INPUT TO NETWORK
+      std::vector<torch::jit::IValue> inputs;
+      torch::Tensor tensor_image = torch::from_blob(resolutionReduced.data, {1,resolutionReduced.rows, resolutionReduced.cols, resolutionReduced.channels() }, at::kByte).to(device).toType(c10::kFloat);
+      tensor_image = tensor_image.permute({ 0,3,1,2 });
+      inputs.push_back(tensor_image);
+      float time2 = std::clock();
+      //PREDICTION FROM SEMANTIC SEGMENTATION NETWORK
+      at::Tensor output = semanticSegmentationModule.forward(inputs).toTensor().argmax(1);
+      at::Tensor cpuTensor = output.to("cpu");
+      float time3 = std::clock();
+      //2 = car
+      //3 = yellow line
+      auto tensor_accessor = cpuTensor.accessor<long,3>();
+      for(int i =0; i<tensor_accessor.size(0);i++){
+        for(int j= 0;j<tensor_accessor.size(1);j++){
+          for(int k =0;k<tensor_accessor.size(2);k++){
+            long pixelValue = tensor_accessor[i][j][k];
+            if(pixelValue ==2){
+              foundCar = true;
+              if(j<minY){
+                minY=j;
               }
-           }else{
-              float distance = msg->ranges.at(i)- car_radius; // may need to remove car radius added as test
-              if(distance <car_width /2 * tan(90 - abs(i*msg->angle_increment + msg->angle_min))){ 
-              float velocity = currentSpeed * cos(abs(i*msg->angle_increment + msg->angle_min)); // may need to change back to currentSpeed  * cos(...)
-              float TTC = distance / velocity;
-              if(TTC<smallestTTC){
-                 smallestTTC=TTC;
+              if(j>maxY){
+                maxY=j;
               }
+              if(k<minX){
+                minX=k;
               }
-
-           }
+              if(k>maxX){
+                maxX=k;
+              }
             }
+          }
         }
-        double relativeTTCDistance  = (smallestTTC-0.5) *currentSpeed; // removing .2 can be allowed I added this to give it somewhat of a buffer so it will always have .2 TTC worth of a gap in distance
-        if(currentSpeed ==0){
-            relativeTTCDistance = msg->ranges.at(indexOfZero)-car_radius;
-        }
-        if(relativeTTCDistance<0){
-            relativeTTCDistance =0;
-        }
-        //IDEA TO LATER SELF
-        //may not be ideal. you may need a lower acceleration because the vehicle wont use its max acceleration if it thinks it only needs to go down a little bit in velocity
-        // this assumes it is always using its max acceleartion
-        double allowableSpeed = sqrt(2 * max_acceleration * relativeTTCDistance);
-        if(allowableSpeed<0){
-            allowableSpeed = 0;
-        }
-        if(abs(steeringAngle) >(20.0/180)*M_PI){
-            if(allowableSpeed >2){
-                allowableSpeed = 1;
-            }
-        }
-        else if(abs(steeringAngle) >(30.0/180)*M_PI){
-            if(allowableSpeed >1){
-                allowableSpeed = 0.5;
-            }
-        }
-        //TODOS
-        // MAKE ADJUSTMENTS FOR REALLY BIG HALLYWAYS WHERE CURVE GETS TO CLOSE TO THE WALL
-        // ADD A SPACER ON THE SIDE OF THE CAR THAT LIKES TO STAY A DISTANCE FROM THE WALL
-        // THERE SEEMS TO BE ERROS WHEN IT TRIES TO MAKE SHARP TURNS WHEN IT IS LESS THAN THAT RADIUS OF CURATURE CHECK THAT PART OUT
-        ackermann_msgs::AckermannDriveStamped drive_st_msg;
-        ackermann_msgs::AckermannDrive drive_msg;
-        drive_msg.steering_angle = steeringAngle;
-        drive_msg.speed = allowableSpeed;
-        drive_st_msg.drive = drive_msg;
-        drive_pub.publish(drive_st_msg);
-        // ROS_INFO("Angle of Best Direction %s",std::to_string(angleOfFarthestPoint).c_str());
-        // ROS_INFO("Corrected Steering Angle %s",std::to_string(steeringAngle).c_str());
-        // ROS_INFO("TTC DISTANCE: %s",std::to_string(relativeTTCDistance).c_str());
-        // ROS_INFO("Smallest TTC: %s",std::to_string(smallestTTC).c_str());
-        // ROS_INFO("Old Speed: %s",std::to_string(currentSpeed).c_str());
-        // ROS_INFO("Speed of Car: %s",std::to_string(allowableSpeed).c_str());
-        currentSpeed = allowableSpeed;
-        if(currentSpeed<0.5){
-            currentSpeed = 0.5;
-        }
-
+      }
+      //Steering Angle
+      std::vector<torch::jit::IValue> inputsToLaneFollow;
+      inputsToLaneFollow.push_back(output.toType(c10::kFloat).unsqueeze(0));
+      at::Tensor outputSteeringAngle = laneFollowModule.forward(inputsToLaneFollow).toTensor();
+      steeringAngle = outputSteeringAngle[0].item().toDouble();
+      //Speed
+      std::vector<torch::jit::IValue> inputsToSpeedControl;
+      inputsToSpeedControl.push_back(output.toType(c10::kFloat).unsqueeze(0));
+      at::Tensor outputSpeed = speedControllerModule.forward(inputsToSpeedControl).toTensor();
+      carSpeed = outputSpeed[0].item().toDouble();
+      float time4 = std::clock();
+      ROS_INFO("Time Between Cycles 2-1: %s",std::to_string(time2-time1).c_str());
+      ROS_INFO("Time Between Cycles 3-2: %s",std::to_string(time3-time2).c_str());
+      ROS_INFO("Time Between Cycles 4-3: %s",std::to_string(time4-time3).c_str());
+      //PREPARE FOR CAMERA OUTPUT
+      //DISABLE WHEN RUNNING EXPERIMENTS
+      at::Tensor prediction = output.permute({1,2,0});
+      prediction=prediction.mul(80).clamp(0,255).to(torch::kU8); // add .mul(80) for visualization
+      cv::cuda::GpuMat predictionMat(cv::Size(desiredX, desiredY), CV_8UC1, prediction.data_ptr<uchar>());
+      Mat notResizedMat;
+      predictionMat.download(notResizedMat);
+      cv::resize(notResizedMat,outputMat,Size(outputX,outputY));
     }
-    double angleToAvoid(float distance){
-        float avoidRadius = car_radius * 1.5;
-        return atan(avoidRadius/distance);
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
     }
-    double getSteeringAngleFromRadius(double radiusOfCurvature, double angleOfFarthestPoint){
-        double steeringAngle;
-        if(radiusOfCurvature < car_radius && angleOfFarthestPoint <0){
-            steeringAngle = -1 * max_steering_angle;
+    ackermann_msgs::AckermannDriveStamped drive_st_msg;
+    ackermann_msgs::AckermannDrive drive_msg;
+    if(fastMode){
+        if(carSpeed>1.25){
+          carSpeed=1.25;
         }
-         else if(radiusOfCurvature < car_radius && angleOfFarthestPoint>0){
-            steeringAngle = max_steering_angle;
-        }else{
-            steeringAngle = atan(1/ ( sqrt( (pow(radiusOfCurvature,2) - pow(car_length/2,2)) / pow(car_length,2) ) ) );
+        if(carSpeed<0){
+          carSpeed = 0;
         }
-        if(angleOfFarthestPoint<0){
-            steeringAngle *= -1;
+        if(carSpeed<=1.0){
+          steeringAngle = steeringAngle / 0.75 * 0.9;
         }
-        return steeringAngle;
+        drive_msg.steering_angle = steeringAngle*-0.75;
+        carSpeed = carSpeed+0.75;
+        drive_msg.speed = carSpeed;
+    }else{
+        drive_msg.steering_angle = steeringAngle*-1 * 0.8; // was * 0.8 for normal speed
+        drive_msg.speed = carSpeed;
     }
+    ROS_INFO("Steering Angle %s",std::to_string(steeringAngle).c_str());
+    ROS_INFO("Speed %s",std::to_string(carSpeed).c_str());
+    drive_st_msg.drive = drive_msg;
+    drive_pub.publish(drive_st_msg);
 
-}; // end of class definition
+    //PUBLISH OBJECT DETECTION DATA
+    //TEST DATA
+    default_car::CarObject testObject;
+    if(foundCar){
+      testObject.classID = 2;
+      testObject.minX = minX;
+      testObject.maxX = maxX;
+      testObject.minY = minY;
+      testObject.maxY = maxY;
+      objectsDetected.objects.push_back(testObject);
+    }
+    object_detection_pub.publish(objectsDetected);
 
+    //Upload Mat Image as ROS TOPIC 
+    cv_bridge::CvImage out_msg;
+    out_msg.header   = msg->header; 
+    out_msg.encoding = sensor_msgs::image_encodings::TYPE_8UC1; 
+    out_msg.image    = outputMat;
+
+    // //Upload Mat Image For Recording
+    // //DISABLE WHEN PERFORMING TESTS
+    // Mat rosMsgMat;
+    // cvtColor(outputMat,rosMsgMat,COLOR_GRAY2BGR);
+    // cv_bridge::CvImage out_msg;
+    // out_msg.header   = msg->header; 
+    // out_msg.encoding = sensor_msgs::image_encodings::BGR8; 
+    // out_msg.image    = rosMsgMat;
+
+    // Output modified video stream
+    image_pub_.publish(out_msg.toImageMsg());
+  }
+};
 int main(int argc, char ** argv) {
-    ros::init(argc, argv, "Reactive_Method");
-    ReactiveMethod rw;
-    ros::spin();
-    return 0;
-}
+    ros::init(argc, argv, "lane_follower");
+    LaneFollower rw;
