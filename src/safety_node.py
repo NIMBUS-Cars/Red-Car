c3ec9f94ceb283a5dfb06bc6648d97c7d59f0b62
929937690 <44484569+929937690@users.noreply.github.com>
2022-03-10 20:06:57 -0600

    Update safety_node.py

--- src/safety_node.py
+++ src/safety_node.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Bool
 from ackermann_msgs.msg import AckermannDriveStamped
-
+from ackermann_msgs.msg import AckermannDrive
 
 class Safety(object):
     """
@@ -40,7 +40,8 @@ class Safety(object):
             "/vesc/high_level/ackermann_cmd_mux/input/nav_0", AckermannDriveStamped, queue_size=10)
         #self.brake_bool_pub = rospy.Publisher(
         #    "/brake_bool", Bool, queue_size=10)
-        self.brake_msg = AckermannDriveStamped()
+        # self.brake_msg = AckermannDriveStamped()
+        self.brake_msg = AckermannDrive()
         self.ttc_threshhold = 6
 
     def odom_callback(self, odom_msg):
@@ -90,7 +91,9 @@ class Safety(object):
             # TODO: publish brake message and publish controller bool
             if self.min_ttc < self.ttc_threshhold:
                 print("Min TTC below Threshhold, Apply brake here: ", self.min_ttc)
-                self.brake_msg.drive.speed = 0.0
+                #self.brake_msg.drive.speed = 0.0
+                self.brake_msg.speed = 0.0
+
                 self.brake_pub.publish(self.brake_msg)
                 print("brake_msg when brake: ", brake_msg)
                 #brake_bool.data = True
