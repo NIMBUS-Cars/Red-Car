780b642ccf7fd3d4a1d5e0ba871f1137d26d2d7b
Ryan Brown <45879932+rbrown78137@users.noreply.github.com>
2022-09-21 10:47:17 -0500

    Update steeringAngleFromPhoto.h

--- include/manual_lane_follow/steeringAngleFromPhoto.h
+++ include/manual_lane_follow/steeringAngleFromPhoto.h
@@ -104,7 +104,7 @@ class ManualSteeringControl{
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return 0;
         }
-        return steeringAngle *-1;
+        return steeringAngle;
         
     }
     int laneFinder(vector<vector<double> > wLines, vector<vector<double> > yLines, int lane)
