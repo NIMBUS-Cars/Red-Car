2446cd24aec79ad59d6ea8f5936762616561ad66
Ryan Brown <45879932+rbrown78137@users.noreply.github.com>
2022-10-09 12:49:07 -0500

    Update overtaking.cpp

--- src/overtaking/overtaking.cpp
+++ src/overtaking/overtaking.cpp
@@ -119,7 +119,7 @@ class LaneFollower{
     ackermann_msgs::AckermannDriveStamped drive_st_msg;
     ackermann_msgs::AckermannDrive drive_msg;
     drive_msg.steering_angle = steeringAngle;
-    drive_msg.speed = 1;//carSpeed;
+    drive_msg.speed = 0.7;//carSpeed;
     ROS_INFO("Test Steering Angle %s",std::to_string(steeringAngle).c_str());
     drive_st_msg.drive = drive_msg;
     drive_pub.publish(drive_st_msg);
